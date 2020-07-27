29192cd08082c8f1259edea05cb5c65ef808827c
Shige Takeda <shige.takeda@snowflake.com>
2020-07-27 11:14:15 -0700

    SNOW-175613 change coding style to Google Java Format (#291)
    Follow the change that the company moved all Java coding style to Google Java Format.

--- src/test/java/net/snowflake/client/core/SessionUtilTest.java
+++ src/test/java/net/snowflake/client/core/SessionUtilTest.java
@@ -4,44 +4,46 @@
 
 package net.snowflake.client.core;
 
-import org.junit.Test;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class SessionUtilTest
-{
+import org.junit.Test;
 
-  /**
-   * Test isPrefixEqual
-   */
+public class SessionUtilTest {
+
+  /** Test isPrefixEqual */
   @Test
-  public void testIsPrefixEqual() throws Exception
-  {
-    assertThat("no port number",
-               SessionUtil.isPrefixEqual(
-                   "https://***REMOVED***/blah",
-                   "https://***REMOVED***/"));
-    assertThat("no port number with a slash",
-               SessionUtil.isPrefixEqual(
-                   "https://***REMOVED***/blah",
-                   "https://***REMOVED***"));
-    assertThat("including a port number on one of them",
-               SessionUtil.isPrefixEqual(
-                   "https://***REMOVED***/blah",
-                   "https://***REMOVED***/"));
+  public void testIsPrefixEqual() throws Exception {
+    assertThat(
+        "no port number",
+        SessionUtil.isPrefixEqual(
+            "https://***REMOVED***/blah",
+            "https://***REMOVED***/"));
+    assertThat(
+        "no port number with a slash",
+        SessionUtil.isPrefixEqual(
+            "https://***REMOVED***/blah",
+            "https://***REMOVED***"));
+    assertThat(
+        "including a port number on one of them",
+        SessionUtil.isPrefixEqual(
+            "https://***REMOVED***/blah",
+            "https://***REMOVED***/"));
 
     // negative
-    assertThat("different hostnames",
-               !SessionUtil.isPrefixEqual(
-                   "https://***REMOVED***/blah",
-                   "https://***REMOVED***/"));
-    assertThat("different port numbers",
-               !SessionUtil.isPrefixEqual(
-                   "https://***REMOVED***/blah",
-                   "https://***REMOVED***/"));
-    assertThat("different protocols",
-               !SessionUtil.isPrefixEqual(
-                   "http://***REMOVED***/blah",
-                   "https://***REMOVED***/"));
+    assertThat(
+        "different hostnames",
+        !SessionUtil.isPrefixEqual(
+            "https://***REMOVED***/blah",
+            "https://***REMOVED***/"));
+    assertThat(
+        "different port numbers",
+        !SessionUtil.isPrefixEqual(
+            "https://***REMOVED***/blah",
+            "https://***REMOVED***/"));
+    assertThat(
+        "different protocols",
+        !SessionUtil.isPrefixEqual(
+            "http://***REMOVED***/blah",
+            "https://***REMOVED***/"));
   }
 }
